b84aa92daa81a4887676031d9a74c296698cd39a
Robert Petz <rpetz@ignitelabs.net>
2025-04-08 16:29:10 -0700

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,31 @@ int Test() {
 	return 1;
 }
 
+// GLX Context Extensions (for OpenGL versions > 2.1)
+typedef struct {
+    int contextMajorVersion;
+    int contextMinorVersion;
+    int contextFlags;
+    int profileMask;
+} GLXContextAttributes;
+
+PFNGLXCREATECONTEXTATTRIBSARBPROC glXCreateContextAttribsARB = 0;
+
+GLXContext createGLXContext(Display *display, GLXFBConfig config, GLXContext shareList, Bool direct, GLXContextAttributes attribs) {
+    int attribList[] = {
+        0x2091, attribs.contextMajorVersion, // GLX_CONTEXT_MAJOR_VERSION_ARB
+        0x2092, attribs.contextMinorVersion, // GLX_CONTEXT_MINOR_VERSION_ARB
+        0x9126, attribs.profileMask,         // GLX_CONTEXT_PROFILE_MASK_ARB
+        0
+    };
+
+    if (!glXCreateContextAttribsARB) {
+        glXCreateContextAttribsARB = (PFNGLXCREATECONTEXTATTRIBSARBPROC)glXGetProcAddressARB((const GLubyte *) "glXCreateContextAttribsARB");
+    }
+
+    return glXCreateContextAttribsARB(display, config, shareList, direct, attribList);
+}
+
 */
 import "C"
 import (
