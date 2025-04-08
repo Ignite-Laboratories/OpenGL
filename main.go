3efa5bb5c07349dd8763113dc2276923b80910e8
Robert Petz <rpetz@ignitelabs.net>
2025-04-08 16:27:36 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 /*
+#cgo LDFLAGS: -lGL -lX11
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <stdlib.h>
+
 int Test() {
 	return 1;
 }
