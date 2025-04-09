25fe3cfff183e35133a4b5db3a87ac4abf65e0c1
Robert Petz <rpetz@ignitelabs.net>
2025-04-09 14:33:36 -0700

    Update main.go

--- main.go
+++ main.go
@@ -88,7 +88,7 @@ func HandleWindowEvent(windowID uint32) {
 	for _, control := range Windows {
 		if control.WindowID == windowID {
 			control.Alive = false
-			time.Sleep(time.Millisecond * 16)
+			time.Sleep(time.Millisecond * 32)
 			control.Window.Destroy()
 		}
 	}
